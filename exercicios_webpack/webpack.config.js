dbc033a91a8cd0f460700438dbb952d53df0ff53
vmarcosp <marcosvcontato.mv@gmail.com>
2017-06-27 22:03:14 -0300

    Configurando estrutura do projeto

---
+++ exercicios_webpack/webpack.config.js
@@ -0,0 +1,10 @@
+'use strict';
+const webpack = require('webpack');
+
+module.exports = {
+    entry:'./ex/index.js',
+    output:{
+        path:`${__dirname}/public`,
+        filename:'./bundle.js'
+    }
+};
